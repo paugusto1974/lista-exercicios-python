2302298c5bf4ed1ec3e2e421cc1943be09d8e037
Paulo Augusto <pauglima@gmail.com>
2021-05-23 19:12:09 -0300

    Create exercicio_1_1.py

---
+++ exercicio_1_1.py
@@ -0,0 +1,10 @@
+"""
+Faça um Programa que mostre a mensagem 'Alo mundo' na tela. 
+"""
+# Forma mais elementar de exibir uma mensagem
+print('Alô, Mundo!')
+# Agora uma forma um pouco melhor, acho...
+# Atribuir uma mensagem do tipo 'string' a uma variável nomeada 'msg'.
+msg = 'Fiat Lux!'
+# Fazer o programa exibir a mensagem guardada na variável
+print(msg)
